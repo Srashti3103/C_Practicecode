e47153ad1195794ab98a71af4912fd8f5ea053ee
Srashti3103 <srashtipatel31@gmail.com>
2025-10-15 10:27:42 +0530

    kth largest and smallest

---
+++ ARRAY/Kth_largest_and_smallest.c
@@ -0,0 +1,44 @@
+// kth largest and smallest element
+#include <stdio.h>
+
+int main() {
+    int m, k;
+
+    // Input: size of array
+    printf("Enter number of elements: ");
+    scanf("%d", &m);
+
+    int arr[m];  
+
+    // Input: elements of array
+    printf("Enter elements:\n");
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Input: K value
+    printf("Enter the value of K: ");
+    scanf("%d", &k);
+
+    // Check for valid K
+    if (k <= 0 || k > m) {
+        printf("Invalid value of K.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < m - 1; i++) {
+        for (int j = i + 1; j < m; j++) {
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    // Output kth smallest and largest elements
+    printf("%dth smallest Element = %d\n", k, arr[k - 1]);
+    printf("%dth largest  Element = %d\n", k, arr[m - k]);
+
+    return 0;
+}
